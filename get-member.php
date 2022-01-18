1763fc733d2a0f8641d5cb0426d29e4ee6cc5013
Wachira Duangdee <wcrcorp@gmail.com>
2022-01-18 19:38:07 +0700

    Create get-member.php

---
+++ get-member.php
@@ -0,0 +1,33 @@
+<?php
+$adServer = "ldap://172.16.1.50/";
+//$adServerSSL = "ldaps://172.16.1.50/";
+$ldapport = 389;
+//$ldapportSSL = 636;
+
+$username = "admininno";
+$password = "1234";
+$domain = "BEMIS";
+
+$ldap = ldap_connect($adServer, $ldapport)  or die("Could not connect to $ldaphost");
+
+$ldaprdn = $domain . "\\" . $username;
+ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
+ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);
+$bind = @ldap_bind($ldap, $ldaprdn, $password);
+
+if ($bind) {
+    $filter = "(sAMAccountName=$username)";
+    $result = ldap_search($ldap, "dc={$domain},dc=COM", $filter);
+    // ldap_sort($ldap, $result, "sn");
+    $info = ldap_get_entries($ldap, $result);
+
+    echo "<pre>";
+    // print_r($info);
+    print_r($info[0]['memberof']);
+    // print_r($info[0]['memberof'][2]);
+
+    @ldap_close($ldap);
+} else {
+    $msg = "Invalid email address / password";
+    echo $msg;
+}
